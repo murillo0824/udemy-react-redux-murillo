94d05d94ceb62112eb4d329efa18e59450ffce83
土屋将一 <tsuchiyashouichi@tsuchiyashouichinoMacBook-Pro.local>
2021-05-02 18:50:42 +0900

    redux make action

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+//action  Javaスクリプトのクラス
+
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
+
+
+export const increment = () => ({
+  type: "INCREMENT",
+});
+
+export const decrement = () => ({
+  type: "DECREMENT",
+});
