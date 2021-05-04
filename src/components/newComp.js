372cebd6007015433f4e7712f307f87edf0b9bfd
土屋将一 <tsuchiyashouichi@tsuchiyashouichinoMacBook-Pro.local>
2021-05-04 23:11:07 +0900

    make component with props name list

--- src/components/newComp.js
+++ src/components/newComp.js
@@ -1,9 +1,37 @@
 import React, { Component } from "react";
 
+
+const nameList = [
+  {
+    name:"murlllo",
+    age:"26",
+    sex:"male"
+  },
+  {
+    name:"shoko",
+    age:"33",
+    sex:"female"
+  }
+]
+
 class NewComp extends Component {
   render() {
-    return <div>テスト</div>;
+    return (
+      <React.Fragment>
+        <h1>名前リスト</h1>
+        {
+          nameList.map((value, index)=>{
+            return <NameList name={value.name} age={value.age} sex={value.sex} key={index} />
+          })
+        }
+      </React.Fragment>
+    );
   }
+}
+
+
+const NameList = (props)=>{
+  return <div>name:{props.name} , age:{props.age}, sex:{props.age}</div>
 }
 
 export default NewComp;
